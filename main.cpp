b36fd3ca93ffa72d0a3aa9ad675d6d54e9642a32
Gaurav Chawla <gauravchawla0003@gmail.com>
2021-10-17 22:51:47 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp  
+//
+//  Created by Gaurav Chawla on 17/10/21.
+//
+
+#include "parallel_estimation_of_pi.h"
+
+int main()
+{
+    serial();
+    parallel();
+    return 0;
+}
+
+
+
